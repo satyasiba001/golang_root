d8b55f8136d635d80bf65b76e772a8cb8e5bbc1c
Satyasiba  Sahoo <satyasiba.sahoo@iserveu.co.in>
2024-07-03 19:28:15 +0530

    added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/satyasiba001/golang_root
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -3,21 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	//var s string = "Hello satya"
-	var g string
-	fmt.Println(g)
-	g = "satya"
-	//k = 10
-	//fmt.Println(k)
-	k := 12.432
-	fmt.Printf("%v\t%T\n",k,k)
-	fmt.Println(g)
-	a,b,c := 0,1,2
-	fmt.Println(a,b,c,"\v")
-	var d,e,f = 4, true ,"satya"
-	fmt.Println(d,e,f)
-	fmt.Printf("%T %T %T \n",d,e,f)
-	j := int(k)
-	//n := bool(k)
-	fmt.Printf("%v\t%T\n",j,j)
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", HomeHandler)
+	r.HandleFunc("/articles", Articlehandler)
+	http.Handle("/",r)
 }
